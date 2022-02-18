bfdfc90a598b12699430ee7a3f3b204fc38fafc8
Alejandro Waldo S <alejandro.waldo5978@alumnos.udg.mx>
2022-02-18 10:52:30 -0600

    secret key in env

--- correos/settings.py
+++ correos/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hv8)h7liov#(%7h72+y(^_kz-v*!tbu62o*hos!y9cskhv-e+%'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
